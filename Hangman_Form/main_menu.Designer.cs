6f9200e3019407889bb79bc933864aec66f80568
Simon Kaminski <munchkinnm@gmail.com>
2020-03-13 00:36:46 -0700

    Added main files
    Added main files for the basis of the project

---
+++ Hangman_Form/main_menu.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Hangman_Form
+{
+    partial class Hangman
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHangman = new System.Windows.Forms.Label();
+            this.btnEasy = new System.Windows.Forms.Button();
+            this.btnMedium = new System.Windows.Forms.Button();
+            this.btnHard = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblHangman
+            // 
+            this.lblHangman.AutoSize = true;
+            this.lblHangman.Font = new System.Drawing.Font("Tw Cen MT", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHangman.Location = new System.Drawing.Point(14, 9);
+            this.lblHangman.Name = "lblHangman";
+            this.lblHangman.Size = new System.Drawing.Size(294, 74);
+            this.lblHangman.TabIndex = 0;
+            this.lblHangman.Text = "Hangman";
+            // 
+            // btnEasy
+            // 
+            this.btnEasy.Location = new System.Drawing.Point(165, 114);
+            this.btnEasy.Name = "btnEasy";
+            this.btnEasy.Size = new System.Drawing.Size(75, 23);
+            this.btnEasy.TabIndex = 1;
+            this.btnEasy.Text = "Easy";
+            this.btnEasy.UseVisualStyleBackColor = true;
+            this.btnEasy.Click += new System.EventHandler(this.btnEasy_Click);
+            // 
+            // btnMedium
+            // 
+            this.btnMedium.Location = new System.Drawing.Point(165, 143);
+            this.btnMedium.Name = "btnMedium";
+            this.btnMedium.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnMedium.Size = new System.Drawing.Size(75, 23);
+            this.btnMedium.TabIndex = 2;
+            this.btnMedium.Text = "Medium";
+            this.btnMedium.UseVisualStyleBackColor = true;
+            this.btnMedium.Click += new System.EventHandler(this.btnMedium_Click);
+            // 
+            // btnHard
+            // 
+            this.btnHard.Location = new System.Drawing.Point(165, 172);
+            this.btnHard.Name = "btnHard";
+            this.btnHard.Size = new System.Drawing.Size(75, 23);
+            this.btnHard.TabIndex = 3;
+            this.btnHard.Text = "Hard";
+            this.btnHard.UseVisualStyleBackColor = true;
+            this.btnHard.Click += new System.EventHandler(this.btnHard_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Hangman_Form.Properties.Resources.Hangman_5;
+            this.pictureBox1.Location = new System.Drawing.Point(27, 86);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(132, 149);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Hangman
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 269);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnHard);
+            this.Controls.Add(this.btnMedium);
+            this.Controls.Add(this.btnEasy);
+            this.Controls.Add(this.lblHangman);
+            this.Name = "Hangman";
+            this.Text = "Hangman";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHangman;
+        private System.Windows.Forms.Button btnEasy;
+        private System.Windows.Forms.Button btnMedium;
+        private System.Windows.Forms.Button btnHard;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Hangman_Form/main_menu.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hangman_Form
+{
+    /*
+     * Easy (3-4 characters)
+     * Medium (5-6 characters)
+     * Hard (7+ characters)
+     */
+    public partial class Hangman : Form
+    {
+        public Hangman()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEasy_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMedium_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnHard_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
