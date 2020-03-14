96d3956834c034b924c4be73a84bb5fa7cd1ad56
Simon Kaminski <munchkinnm@gmail.com>
2020-03-14 14:56:57 -0700

    started on guess system
    Prints underscores in place for letters
    if player enters word entiry the game ends
    some minor errors

--- Hangman_Form/game.Designer.cs
+++ Hangman_Form/game.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.lblCurrentGuess = new System.Windows.Forms.Label();
             this.txtPlayerInput = new System.Windows.Forms.TextBox();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.lblGuesses = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblCurrentGuess
@@ -49,11 +51,31 @@
             this.txtPlayerInput.Size = new System.Drawing.Size(148, 20);
             this.txtPlayerInput.TabIndex = 1;
             // 
+            // btnGuess
+            // 
+            this.btnGuess.Location = new System.Drawing.Point(100, 103);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(75, 23);
+            this.btnGuess.TabIndex = 2;
+            this.btnGuess.Text = "Enter";
+            this.btnGuess.UseVisualStyleBackColor = true;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // lblGuesses
+            // 
+            this.lblGuesses.AutoSize = true;
+            this.lblGuesses.Location = new System.Drawing.Point(0, 0);
+            this.lblGuesses.Name = "lblGuesses";
+            this.lblGuesses.Size = new System.Drawing.Size(0, 13);
+            this.lblGuesses.TabIndex = 3;
+            // 
             // game
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(279, 138);
+            this.Controls.Add(this.lblGuesses);
+            this.Controls.Add(this.btnGuess);
             this.Controls.Add(this.txtPlayerInput);
             this.Controls.Add(this.lblCurrentGuess);
             this.Name = "game";
@@ -67,5 +89,7 @@
 
         private System.Windows.Forms.Label lblCurrentGuess;
         private System.Windows.Forms.TextBox txtPlayerInput;
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.Label lblGuesses;
     }
 }
--- Hangman_Form/game.cs
+++ Hangman_Form/game.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.IO; 
+using System.IO;
 using System.Windows.Forms;
 using System.Threading;
 
@@ -7,23 +7,67 @@ namespace Hangman_Form
 {
     public partial class game : Form
     {
-        string Word { get; set; }
-        string[] LettersInWord { get; set; }
+        static string Word { get; set; }
+        static string[] LettersInWord { get; set; }
         public void printGuesses()
         {
-            //Write an underscore for every letter that is not correctly guessed
-            foreach(string item in LettersInWord)
+            for(int i = 0; i < LettersInWord.Length; i++)
             {
-                lblCurrentGuess.Text += "_";
+                if (LettersInWord[i] != " ")
+                {
+                    lblCurrentGuess.Text += "_";
+                }
+                else if (LettersInWord[i] == " ")
+                {
+                    lblCurrentGuess.Text += " ";
+                }
             }
         }
+        private static int guessAmount = 10; //Declares how many guesses the player has in total
+        string[] allGuesses = new string[guessAmount];
         public game(string word, string[] lettersInWord)
         {
             InitializeComponent();
             Word = word;
             LettersInWord = lettersInWord;
             lblCurrentGuess.Text = "";
+            lblGuesses.Text = "Guesses: " + guessAmount.ToString();
+
             printGuesses();
+            Console.WriteLine("The word is: {0}", Word);
+        }
+
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            string guess = txtPlayerInput.Text;
+            if (guessAmount > 0) //If the player still has guesses left
+            {
+                if (guess.Length > 1 && guess != Word)
+                {
+                    MessageBox.Show("Please enter a valid guess\n1.) Single letters\n2.) Correct word as a whole");
+                }
+                else
+                {
+                    if(txtPlayerInput.Text == Word)//If the player guesses the word in one try
+                    {
+                        lblCurrentGuess.Text = "";
+                        foreach(string letter in LettersInWord)
+                        {
+                            lblCurrentGuess.Text += letter;
+                        }
+                        MessageBox.Show("You guessed the word!");
+                        Environment.Exit(1);
+                    }
+                    Console.WriteLine("Guess: {0}\nvalid guess\n~~~~~~~~~~~~~", guess);
+                    guessAmount--;
+                }
+                lblGuesses.Text = "Guesses: " + guessAmount.ToString();
+            }
+            else //The player is all out of guesses
+            {
+                MessageBox.Show("You are out of guesses\nThe Word was: " + Word);
+                Environment.Exit(1);
+            }
         }
     }
 }
